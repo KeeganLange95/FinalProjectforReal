af06c72184d04d7ce38878f34d5010eaf2ea90cb
KeeganLange95 <kclange@svsu.edu>
2025-04-24 00:59:04 -0400

    fixed bug that wouldn't let you login

--- routes/auth.js
+++ routes/auth.js
@@ -5,19 +5,19 @@ router.post("/login", async (req, res) => {
     try {
         const userData = await User.findOne({where: {username: req.body.username}});
 
-        if(userData) {
-            res.status(400).json({message: "incorrect username or password, please try again"});
+        if(!userData) {
+            res.status(400).json({message: "incorrect username , please try again"});
             return;
         }
 
         const validPassword = userData.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res.status(400).json({message: "incorrect username or password, please try again"});
+            res.status(400).json({message: "incorrect password, please try again"});
             return;
         }
 
-        req.sessions.save(() => {
+        req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
 
