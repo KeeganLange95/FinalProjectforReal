4fc4faaa1bbee15b05b91bde6e47953ba848a87f
KeeganLange95 <kclange@svsu.edu>
2025-04-25 18:37:04 -0400

    Comment

--- routes/home.js
+++ routes/home.js
@@ -1,55 +1,78 @@
 const router = require("express").Router();
+const { response } = require("express");
 const withAuth = require("../middleware/auth");
 const { User } = require("../models");
 
-router.get("/TeamBuilder", withAuth, async (req,res) => {
-    res.render("TeamBuilder", {
-        title: "TeamBuilder", 
-        logged_in: req.session.logged_in
-    });
+router.get("/TeamBuilder", withAuth, async (req, res) => {
+  const getClicked = (id) => {
+    console.log(id);
+  };
+  try {
+    await fetch("http://localhost:3000/characters", { method: "GET" })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("failed to load api");
+        }
+      })
+      .then((data) => {
+        const apiInfo = data;
+
+        res.render("TeamBuilder", {
+          title: "TeamBuilder",
+          apiInfo,
+          logged_in: true,
+          getClicked,
+        });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/profile", withAuth, async (req, res) => {
-    try {
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: {exclude: ["password"]}
-        });
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+    });
 
-        const user = userData.get({plain: true});
+    const user = userData.get({ plain: true });
 
-        res.render("profile", {
-            title: "My Profile", 
-            user,
-            logged_in: true
-        });
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
+    res.render("profile", {
+      title: "My Profile",
+      user,
+      logged_in: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.get("/", (req, res) => {
-    if(req.session.logged_in) {
-        res.redirect("/profile");
-        return;
-    }
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
 
-    res.render("login", {
-        title: "Login",
-        logged_in: req.session.logged_in
-    });
+  res.render("login", {
+    title: "Login",
+    logged_in: req.session.logged_in,
+  });
 });
 
-router.get("/login", (req,res) => {
-    if(req.session.logged_in) {
-        res.redirect("./profile");
-        return
-    }
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("./profile");
+    return;
+  }
 
-    res.render("login", {
-        title: "login",
-        logged_in: req.session.logged_in
-    });
+  res.render("login", {
+    title: "login",
+    logged_in: req.session.logged_in,
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,52 +4,74 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const helmet = require("helmet");
+const fs = require("fs-extra");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./routes");
 const sequelize = require("./config/database");
+const CharactersFile = "Characters.json";
 //const helpers = require("./utils/helpers");
 const { strict } = require("assert");
+const { json } = require("sequelize");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const hbs = exphbs.create();
-
-const sess = {
-    secret: process.env.SESSION_SECRET,
-    Cookie: {
-        secure:process.env.NODE_ENV === "production",
-        httpOnly: true,
-        sameSite: strict,
-        maxAge: 24*60*60*1000 //1 day
+const hbs = exphbs.create({
+  helpers: {
+    json: (context) => {
+      return JSON.stringify(context);
     },
-    resave: false,
-    saveUninitialized: false,
-    store: new SequelizeStore ({
-        db: sequelize
-    })
+  },
+});
+
+const getCharacters = () => {
+  return fs.readJsonSync(CharactersFile, { throws: false }) || [];
 };
 
-app.use(helmet({
+app.get("/characters", (req, res) => {
+  const characters = getCharacters();
+  res.json(characters);
+});
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  Cookie: {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: strict,
+    maxAge: 24 * 60 * 60 * 1000, //1 day
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(
+  helmet({
     contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'"],
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-            styleSrc: ["'self'", "'unsafe-inline'"],
-        },
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+      },
     },
-}));
+  })
+);
 
 app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-sequelize.sync({force: false}).then(() => {
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+  );
 });
